d70f607d00501ae90485bd4f7eb8bcdf60447c1a
tamim1517 <s225176923@deakin.edu.au>
2025-09-14 16:56:58 +1000

    added unit tests

---
+++ backend/test/users.test.js
@@ -0,0 +1,13 @@
+const expect = require("chai").expect;
+const request = require("request");
+
+const baseUrl = "http://localhost:8000";
+
+describe("Users API", function () {
+    it("GET /api/users/me should reject if no token", function (done) {
+        request.get(`${baseUrl}/api/users/me`, function (error, response, body) {
+            expect(response.statusCode).to.not.equal(200);
+            done();
+        });
+    });
+});
